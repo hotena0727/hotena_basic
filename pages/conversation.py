2d7628bd56a181e5452c0510221ecdcdeb70a2af
hotena0727 <kissitai0727@gmail.com>
2026-02-17 21:49:00 +0900

    Delete pages directory

--- pages/conversation.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-
-st.set_page_config(page_title="회화 훈련", layout="centered")
-
-st.title("회화 훈련 페이지")
-st.caption("여기에 회화 훈련 코드를 붙이면 됩니다.")
-
-if st.button("← 홈으로", use_container_width=True):
-    st.switch_page("home.py")
