54f68383bac4b710e1e7815ba916946e32616aad
hotena0727 <kissitai0727@gmail.com>
2026-02-23 07:22:12 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -309,7 +309,7 @@ def scroll_to_top(nonce: int = 0):
         </script>
         <!-- nonce:{nonce} -->
         """,
-        height=1,
+        height=0,
     )
 
 def render_floating_scroll_top():
@@ -419,7 +419,7 @@ def render_floating_scroll_top():
 })();
 </script>
         """,
-        height=1,
+        height=0,
     )
 
 render_floating_scroll_top()
